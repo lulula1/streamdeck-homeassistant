aa34718eea73bf90dd7fd83bf8e8e0495ccc6706
lulula1 <lulula1dev@gmail.com>
2022-11-16 21:30:36 +0100

    Add better resolve of out directory

--- vite-pi.config.ts
+++ vite-pi.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: "./fr.lulula1.streamdeck.homeassistant.sdPlugin",
+    outDir: resolve(process.env.APPDATA || '.', 'Elgato/StreamDeck/Plugins/fr.lulula1.streamdeck.homeassistant.sdPlugin'),
     emptyOutDir: false,
     minify: false,
     rollupOptions: {
